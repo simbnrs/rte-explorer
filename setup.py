adcead45095b4d59600433a04fa36ccf44c92c8e
Simon Ben Arous <s.ben-arous@kayrros.com>
2020-04-18 17:08:02 +0200

    Licence, and installable

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rte_explorer", # Replace with your own username
+    version="0.0.1",
+    author="Simon Ben Arous",
+    description="Python wrapper for the RTE APIs",
+    long_description_content_type="text/markdown",
+    url="https://github.com/simbnrs/rte-explorer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
